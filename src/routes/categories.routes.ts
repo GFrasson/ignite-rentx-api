c6e86aa3d61624701074793de3bd661c2b674b2e
Gabriel Frasson <bielfc27@gmail.com>
2022-05-19 10:53:57 -0300

    Refactor: improve architecture

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,11 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createCategoryService = new CreateCategoryService(categoriesRepository);
-    createCategoryService.execute({ name, description });
-
-    return response.status(201).send();
-});
-
-categoriesRoutes.get("/", (request, response) => {
-    const all = categoriesRepository.list();
-
-    return response.json(all);
-});
+categoriesRoutes.post("/", (req, res) => createCategoryController.handle(req, res));
+categoriesRoutes.get("/", (req, res) => listCategoriesController.handle(req, res));
 
 export { categoriesRoutes };
