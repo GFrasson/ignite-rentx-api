d3465cd7141cdd0253f4fa840019ebc3bc82bba6
Gabriel Frasson <bielfc27@gmail.com>
2022-06-11 11:44:44 -0300

    User migrations

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import { DataSource, DatabaseType } from "typeorm";
 
+import { User } from "../modules/accounts/entities/User";
 import { Category } from "../modules/cars/entities/Category";
 import { Specification } from "../modules/cars/entities/Specification";
 import { CreateCategories1654777657996 } from "./migrations/1654777657996-CreateCategories";
 import { CreateSpecifications1654866421266 } from "./migrations/1654866421266-CreateSpecifications";
+import { CreateUsers1654953994849 } from "./migrations/1654953994849-CreateUsers";
+import { AlterUserDeleteUsername1654956568094 } from "./migrations/1654956568094-AlterUserDeleteUsername";
 
 const dbType: DatabaseType = "postgres";
 
@@ -15,10 +18,15 @@ const AppDataSource = new DataSource({
     password: process.env.TYPEORM_PASSWORD,
     database: process.env.TYPEORM_DATABASE,
     port: Number(process.env.TYPEORM_PORT),
-    synchronize: true,
+    synchronize: false,
     logging: false,
-    entities: [Category, Specification],
-    migrations: [CreateCategories1654777657996, CreateSpecifications1654866421266],
+    entities: [Category, Specification, User],
+    migrations: [
+        CreateCategories1654777657996,
+        CreateSpecifications1654866421266,
+        CreateUsers1654953994849,
+        AlterUserDeleteUsername1654956568094,
+    ],
 });
 
 // AppDataSource.initialize()
