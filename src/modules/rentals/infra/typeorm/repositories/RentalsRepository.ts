35648fbd4cbc5b241f348b5937bb7e50afe7e589
Gabriel Frasson <bielfc27@gmail.com>
2022-07-19 13:37:58 -0300

    List rentals by user use case

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -64,6 +64,14 @@ class RentalsRepository implements IRentalsRepository {
 
         return rental;
     }
+
+    async findByUser(user_id: string): Promise<Rental[]> {
+        const rentals = await this.repository.findBy({
+            user_id,
+        });
+
+        return rentals;
+    }
 }
 
 export { RentalsRepository };
