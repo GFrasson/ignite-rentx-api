ccec1f4ff172a5acade1bc6e02f9ca0373aa1f05
Gabriel Frasson <bielfc27@gmail.com>
2022-08-09 15:26:38 -0300

    Configure static files

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,8 @@ import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import "@shared/container";
+import upload from "@config/upload";
+
 import swaggerFile from "../../../swagger.json";
 import { AppError } from "../../errors/AppError";
 import { router } from "./routes";
@@ -12,6 +14,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
