f4a7d30b04c01f0661d3633bbcda507fd2d0c2d1
Gabriel Frasson <bielfc27@gmail.com>
2022-09-21 14:44:46 -0300

    Rate Limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,11 +8,14 @@ import upload from "@config/upload";
 
 import swaggerFile from "../../../swagger.json";
 import { AppError } from "../../errors/AppError";
+import { rateLimiter } from "./middlewares/rateLimiter";
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(rateLimiter);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
